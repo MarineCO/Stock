1cd4c3f1c2b66113275fb043983be8f11dad3764
marineco <marinecolonge@gmail.com>
2017-02-06 23:27:07 +0100

    chargement de tous les produits dans la méthode getIndex et renvoi vers la vue

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function getIndex() {
+
+    	$products = \App\Product::all();
+
+    	//dd($products);
+    	return view('products', ['products' => $products]);
+    }
 }
